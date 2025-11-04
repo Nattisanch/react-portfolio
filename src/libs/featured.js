1567b44303ddcc4cd7c483d7f8282b3e83227bb5
Natalie Sanchez <nataliesanchez42204@gmail.com>
2025-11-04 05:53:20 -0500

    test for img

--- src/libs/featured.js
+++ src/libs/featured.js
@@ -4,7 +4,7 @@ const featured =[{
         title: "Career Navigator",
         description: "I created a career navigator for my liftoff project that helps users explore different career paths based on their interests and skills. The app provides personalized recommendations and resources to help users make informed decisions about their future.",
         tech:"EXAMPLE: HTML5,CSS, JS",
-        link: './src/assets/placeholder.png',
+        link: './assets/placeholder.png',
         hard: " TBD.",
         easy: " TBD",
         lightwork:"TBD"
--- src/libs/resume.js
+++ src/libs/resume.js
@@ -5,19 +5,19 @@ const projects = [
         title: "Power Factor",
         description: "I created a simple python script into an tkinter App for my internship at PECO, this app has various features such as, calculating data to find the daily 30 minute interval for our customers, an upload file feature, it also saves the data we have calculated for later use I also added a task tracker to help the team stay on track with who is working on what account",
         tech: "Python, Tkinter, Figma,Pandas,Numpy", 
-        link: "./src/assets/powerfactor.png"
+        link: "../assets/powerfactor.png"
     },
     {
         title: "DD draws",
         description: "DD draws is a place where users can learn how to draw my characters I was able to implement AJAX into my work to grab the steps and input it out to the user everytime they click a button. The user can select what character they wish to draw and start learning!",
         tech: "HTML,CSS,Javascript,AJAX", 
-        link: "./src/assets/DDdraws.png"
+        link: "../assets/DDdraws.png"
     }, 
     {
         title: "My portofolio website",
         description: "In my liftoff program, we created a portfolio site as a project to gain hands-on experience with Javascript, HTML, and CSS and further moved it into react. This project also helped us become familiar with the DOM in JavaScript.",
         tech: "HTML5,CSS, JS, React", 
-        link: "./src/assets/port.png"
+        link: "../assets/port.png"
     }
 ];
 
